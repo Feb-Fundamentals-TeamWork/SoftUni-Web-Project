cbb23746b7dac79ad3d738b4cf5911613ee4ef1d
Yavor Ivanov <davko996@gmail.com>
2014-07-03 01:17:30 +0300

    Index page and Videos page updated

--- index.php
+++ index.php
@@ -13,148 +13,31 @@
 	
 ?>
 
-
-
+ <!DOCTYPE html>
 <html>
 	<head>
-		<title></title>
+		<title>Code Burner... A SoftUni Web Project</title>
 		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-
-		<style type="text/css">
-
-			body{
-				background: url('city.jpg') no-repeat center center fixed; 
-				-webkit-background-size: cover;
-				-moz-background-size: cover;
-				-o-background-size: cover;
-				background-size: cover;
-			}
-
-			#contentHolder{
-				width:70%;
-				min-height:135%;
-				background: white;
-				margin-left: auto;
-				margin-right: auto;
-				margin-bottom: 30px;
-				border: 2px solid #E64946;
-				margin-top: 20px;
-			}
-
-			.postsContainer{
-				width: 60%;
-				text-align: center;
-				border-right: 2px solid black;
-				display: inline-block;
-			}
-
-			.postPicture{
-				width: 95%;
-			}
-
-			.picture{
-				width: 90%;
-			}
-
-			.postTitle, .followUsOn{
-				margin-left: 10px;
-				width: 95%;
-				border-bottom: 2px solid red;
-				font-size: 26px;
-				font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif;
-				text-align: left;
-			}
-
-			.postInfo{
-				font-size: 20px;
-				font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif;
-				text-align: right;
-			}
-
-			.postInfo span{
-				color: red;
-			}
-
-			header{
-				width: 70%;
-				margin-right: auto;
-				margin-left: auto;
-				background: #E64946;
-			}
-
-			table{
-				width: 100%;
-				border: none;
-				margin: 0px;
-				padding: 0px;
-			}
-
-			table td{
-				height: 100%;
-				width: 15%;
-				background: #302D2C;
-				color: white;
-				font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif;
-				text-align: center;
-				font-size: 17px;
-				padding: 10px;
-			}
-
-
-			table td:hover{
-				background: #E64946;
-				cursor: pointer;
-			}
-
-			aside{
-				width: 27%;
-				min-height: 100%;
-				display: inline-block;
-				
-				position: absolute;
-			}
-
-			.socialNetworks, .aboutUs{
-				width: 100%;
-				height: 15%;
-
-			}
-
-			.socialIcons{
-				width: 100%;
-				text-align: center;
-			}
-
-			.social{
-				width: 35px;
-				margin-left: 10px;
-			}
-
-			.social:hover{
-				cursor: pointer;
-			}
-
-			#forUs{
-				font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif;
-				text-align: center;
-				font-size: 17px;
-				margin-left: 10px;
-			}
-
-			.headLogo{
-				width: 20%;
-				margin-left: 20px;
-			}
-
-		</style>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		
+<link rel="stylesheet" type="text/css" href="styles.css" />
+		<link rel="stylesheet" type="text/css" href="media-query.css" />
 
 	</head>
 	<body>		
+		<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+				  var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+				  js.src = "//connect.facebook.net/bg_BG/sdk.js#xfbml=1&version=v2.0";
+				  fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));
+			</script>
 		<header>
+			<img src="codeBurner.png" class="headLogo" alt="logo"/> 
+			<input type="text" placeholder="Search..." id="searchBox"/>
 			<table>
-
-				<tr> <img src="codeBurner.png" class="headLogo"/> </tr>
-
 				<tr id="nav">
 					<td onclick="location.href='index.php'">Начало</td>					
 					<td onclick="location.href='pictures.php'">Картинки</td>
@@ -162,7 +45,7 @@
 					<td onclick="location.href='videos.php'">Клипчета</td>
 					<td onclick="location.href='team.php'">Тийм билдинг</td>
 					<td onclick="location.href='events.php'">Събития</td>
-					<td onclick="location.href='about.php'">За нас</td>
+					<td onclick="location.href='http://softuni.bg'">SoftUni</td>
 				</tr>
 			</table>
 		</header>
@@ -173,99 +56,56 @@
 				<?php while($row=mysql_fetch_array($res)) { ?>
 					<div class="postPicture">
 						<h1 class="postTitle"><?php echo $row['title'] ?></h1>
-						<img src="<?php echo $row['image'] ?>" class="picture"/>
+						<img src="<?php echo $row['image'] ?>" class="picture" alt="postImage"/>
 						<div style="display:inline-block; margin-top:-8px;" class="rw-ui-container rw-urid-<?php
 						    echo  $row['post_id'] 
 						?>"></div>
 						<p class="postInfo" style="display:inline-block;"> От: <span><?php echo $row['date'] ?></span> &nbsp;&nbsp;&nbsp; Автор: <span><?php echo $row['autor'] ?></span> </p>
 					</div>
 				<?php } ?>
-
-
-
-				<div class="postPicture">
-					<h1 class="postTitle">You are the... </h1>
-					<img src="shirt.jpg" class="picture"/>
-					<p class="postInfo"> От: <span>15.05.2014</span> &nbsp;&nbsp;&nbsp; Автор: <span>Камелия</span> </p>
-				</div>
-
-				<div class="postPicture">
-					<h1 class="postTitle">They just dont give it to me...</h1>
-					<img src="mug.jpg" class="picture"/>
-					<p class="postInfo"> От: <span>05.05.2014</span> &nbsp;&nbsp;&nbsp; Автор: <span>Zornitza</span> </p>
-				</div>
-
-				<div class="postPicture">
-					<h1 class="postTitle">$26.99 for a watermelon? </h1>
-					<img src="post.jpg" class="picture"/>
-					<p class="postInfo"> От: <span>29.10.2014</span> &nbsp;&nbsp;&nbsp; Автор: <span>Y. Ivanov</span> </p>
-				</div>
-
-				<div class="postPicture">
-					<h1 class="postTitle">$26.99 for a watermelon? </h1>
-					<img src="cssawesome.jpg" class="picture"/>
-					<p class="postInfo"> От: <span>09.11.2014</span> &nbsp;&nbsp;&nbsp; Автор: <span>Petur</span> </p>
-				</div>
 			</div>		
 
 			<aside>
-
-
 				<div class="aboutUs">
 					<h1 class="followUsOn">За нас:</h1>
-
 					<p id="forUs">
-						Нашата мисия е да направим хората, които обучаваме, истински професионалисти в софтуерната индустрия и да им съдействаме в намирането на работа.Нашата мисия е да направим хората, които обучаваме, истински професионалисти в софтуерната индустрия и да им съдействаме в намирането на работа.
-					</p>				
+						Ние сме сайт предлагащ многобройни картинки, видеa, вицове, teamwork идеи и информиращ за всякакви събития свързани в програмирането и информационните технологии.  
+					</p>	
+					<h1 class="followUsOn">&nbsp;</h1>
+					<div class="fb-like-box" style="margin-left:6px;" data-href="https://www.facebook.com/codeburnertw" data-width="360" data-height="280" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>				
 				</div>
+
+				<h1 class="followUsOn">Валидиране:</h1>
+				<p style="display: block; margin-left: 10px;">
+					<a href="http://jigsaw.w3.org/css-validator/check/referer">
+				    <img style="border:0;width:88px;height:31px"
+				        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+				        alt="Valid CSS!" />
+			        </a>
+				    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+				        <img style="border:0;width:88px;height:31px"
+				            src="http://jigsaw.w3.org/css-validator/images/vcss"
+				            alt="Valid CSS!" />
+				    </a>				
+				</p>
 
 				<div class="socialNetworks">
 					<h1 class="followUsOn">Последвайте ни и на:</h1>
 
 					<div class="socialIcons">
-						<img class="social" src="social/fb.png" />
-						<img class="social" src="social/git.png" />
-						<img class="social" src="social/twitter.png" />
-						<img class="social" src="social/instagram.png" />
-						<img class="social" src="social/linkedin.png" />
-						<img class="social" src="social/skype.png" />
+						<img class="social" src="social/fb.png" alt="social"/>
+						<img class="social" src="social/git.png" alt="social" />
+						<img class="social" src="social/twitter.png" alt="social"/>
+						<img class="social" src="social/instagram.png" alt="social"/>
+						<img class="social" src="social/linkedin.png" alt="social"/>
+						<img class="social" src="social/skype.png" alt="social"/>
 					</div>					
 				</div>
 
 			</aside>
 		</div>
 
-
-
-		<script type="text/javascript">(function(d, t, e, m){
-    // Async Rating-Widget initialization.
-    window.RW_Async_Init = function(){
-        RW.init({
-            huid: "177384",
-            uid: "967593d6515266f9fb13b84c74b809d5",
-            source: "website",
-            options: {
-                "urid": "17",
-                "size": "medium",
-                "type": "nero",
-                "style": "thumbs"
-            } 
-        });
-        RW.render();
-    };
-
-    // Append Rating-Widget JavaScript library.
-    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
-        l = d.location, ck = "Y" + t.getFullYear() + 
-        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
-        f = (-1 < l.search.indexOf("DBG=") ? "" : ".min"),
-        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
-    if (d.getElementById(id)) return;              
-    rw = d.createElement(e);
-    rw.id = id; rw.async = true; rw.type = "text/javascript";
-    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
-    s.parentNode.insertBefore(rw, s);
-}(document, new Date(), "script", "rating-widget.com/"));</script>
+		<script type="text/javascript" src="votes-widget.js"></script>
 
 	</body>
 </html>
